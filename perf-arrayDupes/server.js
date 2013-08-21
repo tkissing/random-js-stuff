850d6095c5529d5dd5925b5c5698f70df102466b
Timo Kissing <timo.kissing@gmail.com>
2013-08-21 16:27:19 -0700

    add more optimized versions

--- perf-arrayDupes/server.js
+++ perf-arrayDupes/server.js
@@ -4,7 +4,7 @@ var seed = require('seed-random');
 seed('foo', true);
 
 function randomFromInterval(from,to) {
-    return Math.floor(Math.random() * (to - from + 1) + from);
+	return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
 function createRandomArray(size, max, min) {
@@ -51,11 +51,55 @@ function dupesWithHash(array){
 
 function dupesWithLoops(arr) {
 	var obj = {}, dupes =[];
-	for(var i=0;i < arr.length; i++) {
+	for (var i=0;i < arr.length; i++) {
 		obj[arr[i]] = typeof obj[arr[i]] !== 'undefined' ? obj[arr[i]] + 1 : 0;
 	}
-	for(var prop in obj) {
-		if(obj.hasOwnProperty(prop) && obj[prop] > 0 ){
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop) && obj[prop] > 0 ) {
+			dupes.push(prop);
+		}
+	}
+	return dupes;
+}
+
+function dupesWithHashOptimized(array){
+	var hash = {};
+	var duplicates = [];
+	for (var i = 0, len = array.length; i < len; i++){
+		var item = array[i];
+		var hashLookup = hash[item];
+		if (hashLookup) {
+			if (hashLookup == 1) {
+				duplicates.push(item);
+			}
+			hash[item] = hashLookup + 1;
+		} else {
+			hash[item] = 1;
+		}
+	}
+	return duplicates;
+}
+
+function dupesWithHashOptimized2(array){
+	var hash = {}, duplicates = [], i, item, tmp;
+	for (i = array.length; i--;){
+		item = array[i];
+		tmp = hash[item] || 0;
+		if (tmp == 1) {
+			duplicates[duplicates.length] = item;
+		}
+		hash[item] = tmp + 1;
+	}
+	return duplicates;
+}
+
+function dupesWithLoopsOptimized(arr) {
+	var obj = {}, dupes =[], i, prop;
+	for (i = arr.length; i--;) {
+		obj[arr[i]] = obj[arr[i]] ? 2 : 1;
+	}
+	for (prop in obj) {
+		if (obj[prop] > 1) {
 			dupes.push(prop);
 		}
 	}
@@ -64,7 +108,7 @@ function dupesWithLoops(arr) {
 
 function describeDataSet(array, options) {
 	var hash = {}, dupes = 0, uniqDupes = 0;
-	for(var i = 0, len = array.length; i < len; i++){
+	for (var i = 0, len = array.length; i < len; i++){
 		var item = String(array[i]);
 
 		hash[item] = hash[item] ? hash[item] + 1 : 1;
@@ -86,25 +130,24 @@ function describeDataSet(array, options) {
 }
 
 var sets = [
-	{size: 1e1, max: 1e1},
-	{size: 1e1, max: 1e2},
+{size: 1e1, max: 1e1},
+{size: 1e1, max: 1e2},
 
-	{size: 1e2, max: 1e1},
-	{size: 1e2, max: 1e2},
-	{size: 1e2, max: 1e3},
+{size: 1e2, max: 1e1},
+{size: 1e2, max: 1e2},
+{size: 1e2, max: 1e3},
 
-	{size: 1e3, max: 1e1},
-	{size: 1e3, max: 1e2},
-	{size: 1e3, max: 1e3},
-	{size: 1e3, max: 1e4},
+{size: 1e3, max: 1e1},
+{size: 1e3, max: 1e2},
+{size: 1e3, max: 1e3},
+{size: 1e3, max: 1e4},
 
-	{size: 1e4, max: 1e1},
-	{size: 1e4, max: 1e2},
-	{size: 1e4, max: 1e3},
-	{size: 1e4, max: 1e4},
-	{size: 1e4, max: 1e5}
+{size: 1e4, max: 1e1},
+{size: 1e4, max: 1e2},
+{size: 1e4, max: 1e3},
+{size: 1e4, max: 1e4},
+{size: 1e4, max: 1e5}
 ];
-
 
 sets.forEach(function(v) {
 	var suite = new Benchmark.Suite;
@@ -112,25 +155,34 @@ sets.forEach(function(v) {
 
 	// add tests
 	suite
+	.add('dupesWithIndex', function() {
+		dupesWithIndex(array);
+	})
+	.add('dupesWithLoops', function() {
+		dupesWithLoops(array);
+	})
+	.add('dupesWithLoopsOptimized', function() {
+		dupesWithLoopsOptimized(array);
+	})
+	.add('dupesWithHash', function() {
+		dupesWithHash(array);
+	})
+	.add('dupesWithHashOptimized', function() {
+		dupesWithHashOptimized(array);
+	})
+	.add('dupesWithHashOptimized2', function() {
+		dupesWithHashOptimized2(array);
+	})
+	// add listeners
 	.on('start', function(event) {
 		describeDataSet(array, v);
 	})
-	.add('dupesWithIndex', function() {
-	  dupesWithIndex(array);
-	})
-	.add('dupesWithLoops', function() {
-	  dupesWithLoops(array);
-	})
-	.add('dupesWithHash', function() {
-	  dupesWithHash(array);
-	})
-	// add listeners
 	.on('cycle', function(event) {
-	  console.log(String(event.target));
+		console.log(String(event.target));
 	})
 	.on('complete', function() {
-	  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+		console.log('Fastest is ' + this.filter('fastest').pluck('name'));
 	})
 	// run sync
 	.run({ 'async': false });	
-})
+});
